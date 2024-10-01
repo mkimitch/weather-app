78873c7101412042dc276f8fdba346be603f5872
Mark Kimitch <mark.kimitch@gmail.com>
2024-10-01 12:46:26 -0500

    feat: add SSL support to development server
    Introduced the USE_SSL environment variable and modified webpack.dev.js to conditionally enable HTTPS based on its value.

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,11 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const fs = require('fs')
 const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const useSSL = process.env.USE_SSL === 'true'
 
 module.exports = merge(common, {
 	devServer: {
@@ -10,13 +15,22 @@ module.exports = merge(common, {
 		host: '0.0.0.0',
 		hot: true,
 		port: 8080,
-		server: {
-			options: {
-				cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.crt')),
-				key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.key')),
-			},
-			type: 'https',
-		},
+
+		server: useSSL
+			? {
+					type: 'https',
+					options: {
+						cert: fs.readFileSync(
+							path.resolve(__dirname, 'certs/localhost.crt')
+						),
+						key: fs.readFileSync(
+							path.resolve(__dirname, 'certs/localhost.key')
+						),
+					},
+			  }
+			: {
+					type: 'http',
+			  },
 		static: './dist',
 	},
 	devtool: 'inline-source-map',
